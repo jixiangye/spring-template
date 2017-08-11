e888d0603f48e094797f7fd08c0cb3f45dfc6882
yejx <yejx@servyou.com.cn>
2017-08-11 11:53:42 +0800

    提交2

--- src/main/java/com/yjx/template/controller/IndexController.java
+++ src/main/java/com/yjx/template/controller/IndexController.java
@@ -13,6 +13,7 @@ public class IndexController {
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public String index() {
         System.out.println(1);
+        System.out.println(2);
         return "index";
     }
 
