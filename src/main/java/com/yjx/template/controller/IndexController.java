d477639b2e32cd8b366bb163f172da9a00c13a95
yejx <yejx@servyou.com.cn>
2017-08-17 21:12:11 +0800

    新增test

--- src/main/java/com/yjx/template/controller/IndexController.java
+++ src/main/java/com/yjx/template/controller/IndexController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IndexController {
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public String index() {
+        System.out.println("新增test");
         System.out.println(1);
         System.out.println(2);
         return "index";
